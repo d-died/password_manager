fe3d948a84571dbc0c10583a3749955b9b63d67b
Derek Diedrich <dsumpmann@gmail.com>
2022-08-16 14:11:22 -0600

    final commit

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint, choice, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    random_letters = [choice(letters) for letter in range(randint(8, 10))]
+    random_numbers = [choice(numbers) for number in range(randint(2, 4))]
+    random_symbols = [choice(symbols) for symbol in range(randint(2, 4))]
+
+    password_list = random_symbols + random_numbers + random_letters
+    shuffle(password_list)
+    password = "".join(password_list)
+    password_input.insert(0, string=password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    if website == "" or password == "":
+        messagebox.showinfo(title="OOPS", message="Please don't leave fields blank!")
+    else:
+        save_details = messagebox.askokcancel(title=website, message=f"The details you entered are as follows: \n Email: {email} \n "
+                                                      f"Password: {password} \n Would you like to save these details?")
+        if save_details:
+            with open("data.txt", "a") as data:
+                data.write(f"{website} | {email} | {password} \n")
+            website_input.delete(0, END)
+            password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -36,30 +67,11 @@ password_label.grid(column=0, row=3)
 password_input = Entry(width=18, bg="white", fg="black")
 password_input.grid(column=1, row=3)
 
-def gen_pass():
-    print("I generated a password hehe")
-
-gen_pass_button = Button(text="Generate Password", highlightbackground="white", command=gen_pass)
+# Generate password button:
+gen_pass_button = Button(text="Generate Password", highlightbackground="white", command=gen_password)
 gen_pass_button.grid(row=3, column=2)
 
-# Add entry:
-def save():
-
-    website = website_input.get()
-    email = email_input.get()
-    password = password_input.get()
-
-    if website == "" or email == "" or password == "":
-        messagebox.showinfo(title="OOPS", message="Please don't leave fields blank!")
-    else:
-        save_details = messagebox.askokcancel(title=website, message=f"The details you entered are as follows: \n Email: {email} \n "
-                                                      f"Password: {password} \n Would you like to save these details?")
-        if save_details:
-            with open("data.txt", "a") as data:
-                data.write(f"{website} | {email} | {password} \n")
-            website_input.delete(0, END)
-            password_input.delete(0, END)
-
+# Add entry to txt file:
 add_button = Button(text="Add", width=36, highlightbackground="white", command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
